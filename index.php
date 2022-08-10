e688980f2c222c90897083bdc3d710a291cb878b
taskportu <task@portu.no>
2022-08-10 09:32:41 +0530

    paragraph add

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Digi</title>
 </head>
+
 <body>
-    <?= "<h1><b><i><u>wooow</u></i></b></h1>" ?>
-    <h1>HELLO DIGIFRONT</h1>
-    <h1>HELLO DIGIFRONT</h1>
-    <h1>HELLO DIGIFRONT</h1>
+    <h1 style="text-align:center;">HELLO DIGIFRONT</h1>
+    <p>
+        fds ajhfbmn s,nfb kskhhbm jhfb jshgfjshbsjh jshgsyfgshfgsj hsgjfg sdjhfgsj hgsj hs gjsgj sg jsg jhhg sg sdjhfgsj fgsjgfsjgfsj dgfjsd gfjsdfgjsddg iu gfjasgf skdgfjsfjs.
+    </p>
 </body>
+
 </html>
