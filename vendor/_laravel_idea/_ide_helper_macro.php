fb94f8fd6f482113c1190a641886ecedf6a37f4d
taskportu <task@portu.no>
2022-08-10 10:18:45 +0530

    vendor added

---
+++ vendor/_laravel_idea/_ide_helper_macro.php
@@ -0,0 +1,21 @@
+<?php
+/** @noinspection PhpUndefinedClassInspection */
+/** @noinspection PhpFullyQualifiedNameUsageInspection */
+
+namespace Illuminate\Http {
+    
+    /**
+     * @method array validate(array $rules, ...$params)
+     * @method bool|string hasValidSignature($absolute = true)
+     */
+    class Request {}
+}
+
+namespace Illuminate\Support {
+    
+    /**
+     * @method void downloadExcel(string $fileName, string $writerType = null, $withHeadings = false)
+     * @method void storeExcel(string $filePath, string $disk = null, string $writerType = null, $withHeadings = false)
+     */
+    class Collection {}
+}
---
+++ vendor/_laravel_idea/_ide_helper_macro_static.php
@@ -0,0 +1,21 @@
+<?php
+/** @noinspection PhpUndefinedClassInspection */
+/** @noinspection PhpFullyQualifiedNameUsageInspection */
+
+namespace Illuminate\Http {
+    
+    /**
+     * @method static array validate(array $rules, ...$params)
+     * @method static bool|string hasValidSignature($absolute = true)
+     */
+    class Request {}
+}
+
+namespace Illuminate\Support {
+    
+    /**
+     * @method static void downloadExcel(string $fileName, string $writerType = null, $withHeadings = false)
+     * @method static void storeExcel(string $filePath, string $disk = null, string $writerType = null, $withHeadings = false)
+     */
+    class Collection {}
+}
